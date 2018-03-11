c8e6783ab9aa1b44ad2dc976babe2ee7bd8b807f
sander frenken <sanderfrenken@sanders-MacBook-Pro-2.local>
2018-03-11 14:47:04 +0100

    Add iOS support to demo, refactored iOS implementation to support reload on property set

--- ReactNativeViewPDF/PDFViewManager.m
+++ ReactNativeViewPDF/PDFViewManager.m
@@ -5,34 +5,35 @@
 RCT_EXPORT_MODULE()
 
 RCT_CUSTOM_VIEW_PROPERTY(resource, NSString, PDFView) {
-  [view setResource: json ? [RCTConvert NSString: json] : nil];
+    [view setResource: json ? [RCTConvert NSString: json] : nil];
+    [view layoutSubviews];
 }
 
 RCT_CUSTOM_VIEW_PROPERTY(resourceType, NSString, PDFView) {
-  [view setResourceType: json ? [RCTConvert NSString: json] : nil];
+    [view setResourceType: json ? [RCTConvert NSString: json] : nil];
 }
 
 RCT_CUSTOM_VIEW_PROPERTY(textEncoding, NSString, PDFView) {
-  [view setTextEncoding: json ? [RCTConvert NSString: json] : UTF_8];
+    [view setTextEncoding: json ? [RCTConvert NSString: json] : UTF_8];
 }
 
 RCT_EXPORT_VIEW_PROPERTY(onLoad, RCTDirectEventBlock)
 RCT_EXPORT_VIEW_PROPERTY(onError, RCTDirectEventBlock)
 
 - (instancetype)init {
-  self = [super init];
-  if ( self ) {
-    self.pdfView = [[PDFView alloc] init];
-  }
-  return self;
+    self = [super init];
+    if ( self ) {
+        self.pdfView = [[PDFView alloc] init];
+    }
+    return self;
 }
 
 - (UIView *)view {
-  return self.pdfView;
+    return self.pdfView;
 }
 
 + (BOOL)requiresMainQueueSetup {
-  return YES;
+    return YES;
 }
 
 @end
