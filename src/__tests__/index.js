5645b48a0ae03ac9c461fed9a64fcbea7e30648c
Maksym Rusynyk <Maksym.Rusynyk@ing.nl>
2018-03-17 13:26:32 +0100

    Unit tests for index.js

--- src/__tests__/index.js
+++ src/__tests__/index.js
@@ -7,9 +7,66 @@ import PDFVIew from '../index';
 jest.mock('../RNPDFView', () => 'RNPDFView');
 
 describe('PDFView', () => {
-  it('renders', () => {
+  it('renders with default url source type', () => {
     const tree = renderer.create(<PDFVIew resource="http://site.com/pdf" />);
 
     expect(tree).toMatchSnapshot();
+  });
+
+  it('renders base64', () => {
+    const tree = renderer.create(<PDFVIew resource="base64" resourceType="base64" />);
+
+    expect(tree).toMatchSnapshot();
+  });
+
+  it('invokes onLoad callback', () => {
+    const onLoad = jest.fn();
+    const tree = renderer.create(<PDFVIew
+      onLoad={onLoad}
+      resource="base64"
+      resourceType="base64"
+    />);
+
+    (tree.toJSON(): any).props.onLoad();
+
+    expect(onLoad).toHaveBeenCalledTimes(1);
+  });
+
+  it('invokes onError callback', () => {
+    const onError = jest.fn();
+    const tree = renderer.create(<PDFVIew
+      onError={onError}
+      resource="base64"
+      resourceType="base64"
+    />);
+
+    (tree.toJSON(): any).props.onError();
+
+    expect(onError).toHaveBeenCalledTimes(1);
+    expect(onError.mock.calls).toMatchSnapshot();
+  });
+
+  it('invokes onError callback and provides native error', () => {
+    const onError = jest.fn();
+    const tree = renderer.create(<PDFVIew
+      onError={onError}
+      resource="base64"
+      resourceType="base64"
+    />);
+
+    (tree.toJSON(): any).props.onError({ nativeEvent: { message: 'native error' } });
+
+    expect(onError).toHaveBeenCalledTimes(1);
+    expect(onError.mock.calls).toMatchSnapshot();
+  });
+
+  it('uses default onLoad and onError callbacks', () => {
+    const tree = renderer.create(<PDFVIew
+      resource="base64"
+      resourceType="base64"
+    />);
+
+    (tree.toJSON(): any).props.onError();
+    (tree.toJSON(): any).props.onLoad();
   });
 });
