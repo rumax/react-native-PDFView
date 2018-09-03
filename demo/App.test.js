eb4a8a9ab48f09080d8af6ffcc02c530e8a047c8
Maksym Rusynyk <Maksym.Rusynyk@ing.nl>
2018-09-03 23:20:33 +0200

    eslint and flow for demo project

--- demo/App.test.js
+++ demo/App.test.js
@@ -1,7 +1,8 @@
+/* @flow */
+import renderer from 'react-test-renderer';
+
 import React from 'react';
 import App from './App';
-
-import renderer from 'react-test-renderer';
 
 it('renders without crashing', () => {
   const rendered = renderer.create(<App />).toJSON();
