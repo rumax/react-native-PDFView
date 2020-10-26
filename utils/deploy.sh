c379d5bdc91738dba88141b8319a630adb40a920
Maksym Rusynyk <m.rusynyk@gmail.com>
2020-10-26 17:05:32 +0100

    Better TypeScript support

--- utils/deploy.sh
+++ utils/deploy.sh
@@ -11,3 +11,9 @@ mkdir lib
 # Remove flow types
 ./node_modules/.bin/flow-remove-types src/index.js --pretty --out-file lib/index.js
 ./node_modules/.bin/flow-remove-types src/RNPDFView.js --pretty --out-file lib/RNPDFView.js
+
+# TypeScript
+cp ./index.d.ts ./lib
+
+# FlowType
+./node_modules/.bin/flow-copy-source --ignore __tests__/* --verbose src lib
